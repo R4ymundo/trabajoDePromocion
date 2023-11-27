5493653543aa4bb5f400e07c8d97c34612e021d1
Sanluki04 <santiaaronp04@gmail.com>
2023-11-27 17:54:18 -0300

    cree cargarPartidas y seleccionarOpcion

--- programaCarreraPerez.php
+++ programaCarreraPerez.php
@@ -31,9 +31,43 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
-/* ****COMPLETAR***** */
+/**
+ * Obtiene una colección de partidas jugadas
+ * @return array
+ */
+function cargarPartidas(){
+    $coleccionPartidas[0] = ["palabraWordix "=> "QUESO" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 0,];
+    $coleccionPartidas[1] = ["palabraWordix "=> "CASAS" , "jugador" => "rudolf", "intentos"=> 3, "puntaje" => 14];
+    $coleccionPartidas[2] = ["palabraWordix "=> "QUESO" , "jugador" => "pink2000", "intentos"=> 6, "puntaje" => 10];
+    $coleccionPartidas[3] = ["palabraWordix "=> "VERDE" , "jugador" => "sebas", "intentos"=> 0, "puntaje" => 0,];
+    $coleccionPartidas[4] = ["palabraWordix "=> "VARON" , "jugador" => "rodolfo", "intentos"=> 4, "puntaje" => 14];
+    $coleccionPartidas[5] = ["palabraWordix "=> "RATON" , "jugador" => "gustavo", "intentos"=> 2, "puntaje" => 10];
+    $coleccionPartidas[6] = ["palabraWordix "=> "HIELO" , "jugador" => "roberto", "intentos"=> 0, "puntaje" => 0,];
+    $coleccionPartidas[7] = ["palabraWordix "=> "PISOS" , "jugador" => "sebas", "intentos"=> 3, "puntaje" => 14];
+    $coleccionPartidas[8] = ["palabraWordix "=> "HIELO" , "jugador" => "nico", "intentos"=> 2, "puntaje" => 10];
+    $coleccionPartidas[9] = ["palabraWordix "=> "FERIA" , "jugador" => "jere", "intentos"=> 3, "puntaje" => 14];
+    return $coleccionPartidas;
+}
 
-
+/**
+ * Obtiene una colección de palabras
+ * @return INT 
+ */
+function seleccionarOpcion(){
+        do{
+            echo "Menú de opciones: 
+            1) Jugar al wordix con una palabra elegida 
+            2) Jugar al wordix con una palabra aleatoria 
+            3) Mostrar una partida
+            4) Mostrar la primer partida ganadora
+            5) Mostrar resumen de Jugador
+            6) Mostrar listado de partidas ordenadas por jugador y por palabra
+            7) Agregar una palabra de 5 letras a Wordix
+            8) salir \n";
+            $opcion=trim(fgets(STDIN));
+        }while($opcion<0 || $opcion>8);
+    return $opcion;
+}
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
